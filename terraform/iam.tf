ebb7a03c07005a01337eb85d60da0515dd762b49
siluryan <guilhermerrdias@gmail.com>
2026-04-04 21:43:20 -0300

    :lock: security: remover allUsers do serviço n8n no Run

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -4,14 +4,6 @@ resource "google_project_iam_member" "n8n_secretmanager" {
   member  = "serviceAccount:${google_service_account.n8n_runner.email}"
 }
 
-resource "google_cloud_run_v2_service_iam_member" "public_access" {
-  project  = var.project_id
-  location = var.region
-  name     = google_cloud_run_v2_service.n8n.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
-
 resource "google_cloud_run_v2_service_iam_member" "scheduler_invoker" {
   project  = var.project_id
   location = var.region
