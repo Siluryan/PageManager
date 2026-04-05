a731df5eee13fd03ef42370668d41505119ce1c8
siluryan <guilhermerrdias@gmail.com>
2026-04-04 21:07:56 -0300

    :recycle: refactor: separar módulo Terraform em ficheiros por domínio

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,10 @@
 terraform {
   required_version = ">= 1.6"
 
-  backend "gcs" {}
+  backend "gcs" {
+    bucket = "siluryan-page-manager"
+    prefix = "pagemanager/terraform"
+  }
 
   required_providers {
     google = {
